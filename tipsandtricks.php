d58a64bc1fd79befe27c98f27162ac271f208fbc
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2022-12-07 15:28:16 +0200

    07

--- tipsandtricks.php
+++ tipsandtricks.php
@@ -17,42 +17,202 @@ $content = '
             <div class="row mb-30-none justify-content-center">
                 <div class="col-12">
                     <div class="cate-single-item">
-                        <h4 class="title">3step to enable teams meeting add-in from outlook </h4>
-                        <p>STEP1: in outlook select file->options->add-ins->manage,select "disabled items",and then go </p>
-                        <p>STEP2: if you see teams listed under "disabled items select it , and then select Enable "</p>
-                        <p>STEP3: Restart outlook and confirm if the add-in is working</p>
-                        <div class="cate-author">
-                            <div class="thumb">
-                                <a href="#0"><img src="assets/images/cate/single2.png" alt="cate"></a>
-                            </div>
-                            <div class="content">
-                                <h5 class="subtitle"><a href="#0">Lindo Makhubo</a></h5>
-                                <span>FSE ,TUTUKA</span>
-                            </div>
-                            <div class="thumb">
-                                <a href="#"><img src="assets/images/cate/like.png" alt="cate"></a>
-                            </div>
-                        </div>
+                        <h4 class="title">Coolest website of the day: <a href="https://symphony.iohk.io/en/">https://symphony.iohk.io/en/</a></h4>
+                        <p>Visuals can help you understand even the most complex of technologies. Blockchain and cryptocurrency are all the rage now. The Symphony of Blockchain takes you on an interactive, visual and auditory exploration of what its all about. Its a way to mix online fun with a dose of education.</p>
+
+
                     </div>
                 </div>
                 <div class="col-12">
                     <div class="cate-single-item">
-                        <h4 class="title">four clicks to having your teams and sharepoint files synced for working offline </h4>
-                        <p>1.select a channel</p>
-                        <p>2.click on flise tab</p>
-                        <p>3.click open in sharepoint</p>
-                        <p>4.click on sync</p>
-                        <div class="cate-author">
-                            <div class="thumb">
-                                <a href="#0"><img src="assets/images/cate/single2.png" alt="cate"></a>
-                            </div>
-                            <div class="content">
-                                <h5 class="subtitle"><a href="#0">Ross james</a></h5>
-                                <span>FSE,ARNOLT</span>
-                            </div>
-                        </div>
+                        <h4 class="title">Coolest website of the day: <a href="https://www.tis.tv/">https://www.tis.tv/</a></h4>
+                        <p>It showcases the latest videos on innovation and futuristic technologies.<br>
+                        It is a wonderful learning platform. As their charter says, "We aim to inspire and educate by putting innovations and innovators in the spotlight.</p>
+
+
                     </div>
                 </div>
+                <div class="col-12">
+                <div class="cate-single-item">
+                    <h4 class="title">Coolest website of the day: <a href=" https://ncase.me/trust/">https://ncase.me/trust/</a></h4>
+                    <p>Mistrust is as deadly as minformation. And both have been part of or human experience for a long time. This interactive game uses game theory to simplify the problem of mistrust for us.
+                    Game theory itself is a complex subject, so the visualization uses a "cheating" mode and a "cooperation" mode to reveal the neuroscience of mistrust.
+                    </p>
+
+
+                </div>
+            </div>
+            <div class="col-12">
+            <div class="cate-single-item">
+                <h4 class="title">Coolest website of the day: <a href="http://savethesounds.info/">http://savethesounds.info/</a></h4>
+                <p>Want a dose of serious nostalgia? Maybe, the sound of a dial-up modem will do it for you or the dial of a rotary phone. The Museum of Nostalgia will send you back in time. The single-person effort from Brendan Chilcutt aims to preserve the signature sounds of old technologies.</p>
+
+
+            </div>
+        </div>
+        <div class="col-12">
+        <div class="cate-single-item">
+            <h4 class="title">Coolest website of the day: <a href=" https://stumbled.to/"> https://stumbled.to/</a></h4>
+            <p>Remember StumbleUpon? The popular site was shuttered, but several StumbleUpon alternatives rose up in its wake. Stumble works on the same principles of random discovery and that makes it a worthwhile website to go on when you are bored.
+            The links are carefully curated and as of the last count, there are at least 2400+ sites on their radar.</p>
+
+
+        </div>
+    </div>
+
+    <div class="col-12">
+    <div class="cate-single-item">
+        <h4 class="title">Coolest website of the day: <a href=" https://www.duolingo.com/">  https://www.duolingo.com/</a></h4>
+        <p>Make the most of your free time and cross something off your bucket by becoming fluent in a different language with the help of Duolingo! This fun website also doubles as a fun free app on your phone so you never miss a lesson.</p>
+
+
+    </div>
+</div>
+
+<div class="col-12">
+    <div class="cate-single-item">
+        <h4 class="title">Coolest website of the day: <a href="https://lizardpoint.com/"> https://lizardpoint.com/</a></h4>
+        <p>Geography not your best subject in school? Lizard Point makes learning fun with virtual quizzes that help you broaden your horizons without all the stress of tests and final exams.
+        </p>
+
+
+    </div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://www.musictheory.net/">https://www.musictheory.net/</a></h4>
+    <p>Learn the language behind your favorite pieces of music with this fun website! They offer free lessons and exercises so you have all you need to become the next musical genius</p>
+
+
+</div>
+</div>
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://pointerpointer.com/">https://pointerpointer.com/</a></h4>
+    <p>Please hold still while we locate your pointer...</p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href=" https://www.smithsonianmag.com/"> https://www.smithsonianmag.com/</a></h4>
+    <p>If you want to learn something useful about history or technology without feeling like you are in a classroom, this is the place to go. The articles are of highest standard using a language which not too academic. It’s one of the best places to spend time internet</p>
+
+
+</div>
+</div>
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://bigthink.com/">https://bigthink.com//</a></h4>
+    <p>It’s a place which shares ideas and insights from the most successful people around the world. You won’t get life-hacks here, but you will get real information. You will learn from the mistakes of the others and their successes too. A good place to get the content from people who know what they are talking about.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://www.justwatch.com/"> https://www.justwatch.com/</a></h4>
+    <p>With the rise of multiple streaming services, it is no longer easy to locate which platform has the show you’re willing to watch. To help you find where to find your favourite TV shows and movies, there exists a website named Justwatch. All you have to do is type the title of the show and the website will show you where you can watch it</p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://www.worldhistory.org/"> https://www.worldhistory.org/</a></h4>
+    <p>If you love history, this is your one-stop shop to learn everything about our pasts. From Ancient Kingdoms to Epic wars, you can find everything here. It is a great site for history buffs.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href=" http://www.pixelthoughts.co/"> http://www.pixelthoughts.co/</a></h4>
+    <p>It’s a very simple sixty-second meditation tool. There’s a bubble which floats through empty space. You just put in your troubling thought there and follow it for sixty-seconds. This little exercise is really relaxing.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://asoftmurmur.com/">https://asoftmurmur.com/</a></h4>
+    <p>The site hosts various natural sounds like that of rain, wind, waves etc. It helps you relax and focus when you are working. It also has a corresponding app for both iOS and Android.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href=" https://thispersondoesnotexist.com/"> https://thispersondoesnotexist.com/</a></h4>
+    <p>If AI and deep-fakes fascinate you, this is a website that will either make you very excited, or give you nightmares about whether ‘The Matrix’ is real, and if you, at some point in your life, took the blue pill instead of the red one. Either way, the website generates fake people using GAN (or generative adversarial networks), and displays them to you. You can refresh the page to see a different face. Also, if this interests you, you might also like This Cat Does Not Exist. You know, because why stare at human faces when you can look at cats instead.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://littlealchemy2.com/">https://littlealchemy2.com/</a></h4>
+    <p>This one is a fun little time killer. As its name suggests, the website deals with the process of transformation you achieve when you start mixing different things. You start with Earth, Fire, Water, and Air. The goal is to create as many different materials or objects as possible. For example, earth and air will form dust. There are no rules just mix and match your creations to create new ones. You will not even know where your time went.
+    </p>
+
+
+</div>
+</div>
+
+
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="http://www.vsauce.com/"> http://www.vsauce.com/</a></h4>
+    <p>Vsauce is one of the few websites on the internet which makes learning fun and interesting. Whenever you have a little downtime and looking for ways to pass it, just head over to the website to learn interesting things and concepts such as the infinite money paradox, the game you win by losing, the napkin ring problem, and more. And if none of those things made any sense, you should definitely visit the website.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href="https://postsecret.com/"> https://postsecret.com/</a></h4>
+    <p>Post Secret is a website that hosts anonymous secrets that actual people send in on actual postcards! It has all sorts of secrets that you can check out, from childhood embarrassing moments, to regrets, fears, and everything else. It might not look like it, but you can actually get lost, reading secrets sent in by people from around the world. If you want to share your secrets like this, you can also send in a postcard to the address mentioned on the website.
+    </p>
+
+
+</div>
+</div>
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href=" https://www.coolthings.com/"> https://www.coolthings.com/</a></h4>
+    <p>Do you like browsing the web to find cool gadgets and things that you can use in your home? If so, then Cool Things is a website you should definitely bookmark. It hosts collections of articles and information about cool things. This includes gadgets, entertainment, toys, and even inventions. Plus, it’s a great place to get inspiration for gifts as well, whether you’re looking for gifts for men or women.
+    </p>
+
+
+</div>
+</div>
+
+<div class="col-12">
+<div class="cate-single-item">
+    <h4 class="title">Coolest website of the day: <a href=" https://www.patatap.com/">  https://www.patatap.com/</a></h4>
+    <p>A lot of times when you’re bored you just want to do stuff that’s pretty much useless, but still a lot of fun. If that sounds like something you’re interested in, Patatap fits the bill perfectly. This website does one thing — plays sounds and animations based on the keys you’re pressing on your computer’s keyboard. So, you can start typing out random lines on your keyboard and Patatap will reward you with an explosion of fun sounds and animations. It sounds a little ridiculous, but it’s very addictive, especially if you enjoy typing.
+    </p>
+
+
+</div>
+</div>
                 <div class="col-12">
                     <div class="cate-single-item">
                         <h4 class="title">use google chrome extensions in edge </h4>
@@ -86,5 +246,4 @@ $content = '
     </section>
     <!--============= Category Section Ends Here =============-->
 ';
-include 'Template.php'
-?>
+include 'Template.php';
