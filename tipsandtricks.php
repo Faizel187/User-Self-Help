3231747a16ff018ef20fc6be5fb8e2327e4733e2
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2022-12-08 09:31:02 +0200

    8

--- tipsandtricks.php
+++ tipsandtricks.php
@@ -15,41 +15,9 @@ $content = '
     <section class="category-section padding-top padding-bottom">
         <div class="container">
             <div class="row mb-30-none justify-content-center">
-                <div class="col-12">
-                    <div class="cate-single-item">
-                        <h4 class="title">Coolest website of the day: <a href="https://symphony.iohk.io/en/">https://symphony.iohk.io/en/</a></h4>
-                        <p>Visuals can help you understand even the most complex of technologies. Blockchain and cryptocurrency are all the rage now. The Symphony of Blockchain takes you on an interactive, visual and auditory exploration of what its all about. Its a way to mix online fun with a dose of education.</p>
 
 
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="cate-single-item">
-                        <h4 class="title">Coolest website of the day: <a href="https://www.tis.tv/">https://www.tis.tv/</a></h4>
-                        <p>It showcases the latest videos on innovation and futuristic technologies.<br>
-                        It is a wonderful learning platform. As their charter says, "We aim to inspire and educate by putting innovations and innovators in the spotlight.</p>
 
-
-                    </div>
-                </div>
-                <div class="col-12">
-                <div class="cate-single-item">
-                    <h4 class="title">Coolest website of the day: <a href=" https://ncase.me/trust/">https://ncase.me/trust/</a></h4>
-                    <p>Mistrust is as deadly as minformation. And both have been part of or human experience for a long time. This interactive game uses game theory to simplify the problem of mistrust for us.
-                    Game theory itself is a complex subject, so the visualization uses a "cheating" mode and a "cooperation" mode to reveal the neuroscience of mistrust.
-                    </p>
-
-
-                </div>
-            </div>
-            <div class="col-12">
-            <div class="cate-single-item">
-                <h4 class="title">Coolest website of the day: <a href="http://savethesounds.info/">http://savethesounds.info/</a></h4>
-                <p>Want a dose of serious nostalgia? Maybe, the sound of a dial-up modem will do it for you or the dial of a rotary phone. The Museum of Nostalgia will send you back in time. The single-person effort from Brendan Chilcutt aims to preserve the signature sounds of old technologies.</p>
-
-
-            </div>
-        </div>
         <div class="col-12">
         <div class="cate-single-item">
             <h4 class="title">Coolest website of the day: <a href=" https://stumbled.to/"> https://stumbled.to/</a></h4>
@@ -69,32 +37,10 @@ $content = '
     </div>
 </div>
 
-<div class="col-12">
-    <div class="cate-single-item">
-        <h4 class="title">Coolest website of the day: <a href="https://lizardpoint.com/"> https://lizardpoint.com/</a></h4>
-        <p>Geography not your best subject in school? Lizard Point makes learning fun with virtual quizzes that help you broaden your horizons without all the stress of tests and final exams.
-        </p>
 
 
-    </div>
-</div>
-
-<div class="col-12">
-<div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href="https://www.musictheory.net/">https://www.musictheory.net/</a></h4>
-    <p>Learn the language behind your favorite pieces of music with this fun website! They offer free lessons and exercises so you have all you need to become the next musical genius</p>
 
 
-</div>
-</div>
-<div class="col-12">
-<div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href="https://pointerpointer.com/">https://pointerpointer.com/</a></h4>
-    <p>Please hold still while we locate your pointer...</p>
-
-
-</div>
-</div>
 
 <div class="col-12">
 <div class="cate-single-item">
@@ -114,14 +60,7 @@ $content = '
 </div>
 </div>
 
-<div class="col-12">
-<div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href="https://www.justwatch.com/"> https://www.justwatch.com/</a></h4>
-    <p>With the rise of multiple streaming services, it is no longer easy to locate which platform has the show you’re willing to watch. To help you find where to find your favourite TV shows and movies, there exists a website named Justwatch. All you have to do is type the title of the show and the website will show you where you can watch it</p>
 
-
-</div>
-</div>
 
 <div class="col-12">
 <div class="cate-single-item">
@@ -155,23 +94,15 @@ $content = '
 
 <div class="col-12">
 <div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href=" https://thispersondoesnotexist.com/"> https://thispersondoesnotexist.com/</a></h4>
-    <p>If AI and deep-fakes fascinate you, this is a website that will either make you very excited, or give you nightmares about whether ‘The Matrix’ is real, and if you, at some point in your life, took the blue pill instead of the red one. Either way, the website generates fake people using GAN (or generative adversarial networks), and displays them to you. You can refresh the page to see a different face. Also, if this interests you, you might also like This Cat Does Not Exist. You know, because why stare at human faces when you can look at cats instead.
+    <h4 class="title">Coolest website of the day: <a href="https://archive.org/web/"> https://archive.org/web/</a></h4>
+    <p>Check out how your favorite websites looked before they got all sleek. Type in a URL, choose a date, and the Wayback Machine will show you a screenshot of the site from that particular day in history
     </p>
 
 
 </div>
 </div>
 
-<div class="col-12">
-<div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href="https://littlealchemy2.com/">https://littlealchemy2.com/</a></h4>
-    <p>This one is a fun little time killer. As its name suggests, the website deals with the process of transformation you achieve when you start mixing different things. You start with Earth, Fire, Water, and Air. The goal is to create as many different materials or objects as possible. For example, earth and air will form dust. There are no rules just mix and match your creations to create new ones. You will not even know where your time went.
-    </p>
 
-
-</div>
-</div>
 
 
 
@@ -204,43 +135,18 @@ $content = '
 </div>
 </div>
 
+
+
 <div class="col-12">
 <div class="cate-single-item">
-    <h4 class="title">Coolest website of the day: <a href=" https://www.patatap.com/">  https://www.patatap.com/</a></h4>
-    <p>A lot of times when you’re bored you just want to do stuff that’s pretty much useless, but still a lot of fun. If that sounds like something you’re interested in, Patatap fits the bill perfectly. This website does one thing — plays sounds and animations based on the keys you’re pressing on your computer’s keyboard. So, you can start typing out random lines on your keyboard and Patatap will reward you with an explosion of fun sounds and animations. It sounds a little ridiculous, but it’s very addictive, especially if you enjoy typing.
+    <h4 class="title">Coolest website of the day: <a href="https://riverstyx.com/">https://riverstyx.com/</a></h4>
+    <p>Are you interested in Greek mythology? Even if you aren’t, honestly, this website will make you interested. River Styx is a Greek mythological underworld. The eponymously named website takes you through an interactive tour of the river and the underworld, where you will meet a lot of Greek gods and goddesses. The experience is very much like a point and click game where you click on relevant icons and objects to reveal information and more. It’s a fun, and educational, way to spend free time online.
     </p>
 
 
 </div>
 </div>
-                <div class="col-12">
-                    <div class="cate-single-item">
-                        <h4 class="title">use google chrome extensions in edge </h4>
-                        <p> go to edge://extensions/  </p>
-                        <p> enable the "Allow extensions from other stores" toggle at the botom lef  </p>
-                        <div class="cate-author">
-                            <div class="thumb">
-                                <a href="#0"><img src="assets/images/cate/single2.png" alt="cate"></a>
-                            </div>
-                            <div class="content">
-                                <h5 class="subtitle"><a href="#0">Rick ross</a></h5>
-                                <span>MS EDGE,2022</span>
-                            </div>
-                            <nav>
-  <!-- Add class .pagination-lg for larger blocks or .pagination-sm for smaller blocks-->
-  <ul class="pagination">
-    <li class="page-item"> <a class="page-link" href="#" aria-label="Previous"> <span aria-hidden="true">&laquo;</span> <span class="sr-only">Previous</span></a></li>
-    <li class="page-item active"> <span class="page-link"> 1 <span class="sr-only">(current)</span></span></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">4</a></li>
-    <li class="page-item disabled"><a class="page-link" href="#">5</a></li>
-    <li class="page-item"> <a class="page-link" href="#" aria-label="Next"> <span aria-hidden="true">&raquo;</span> <span class="sr-only">Next</span></a></li>
-  </ul>
-</nav>
-                        </div>
-                    </div>
-                </div>
+
             </div>
         </div>
     </section>
